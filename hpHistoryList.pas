9d45b920d053838fa490db26ffbba376ca62b601
pritaeas <pritaeas@hotmail.com>
2013-05-29 20:49:39 +0200

    Added hpHistoryList.

---
+++ hpHistoryList.pas
@@ -0,0 +1,96 @@
+unit hpHistoryList;
+
+(**
+ * @copyright Copyright (C) 2012-2013, Hans Pollaerts
+ * @author    Hans Pollaerts <pritaeas@gmail.com>
+ * @category  Classes
+ * @package   hpHistoryList
+ * @version   1.00
+ *)
+
+(**
+ * History
+ *
+ * 1.00       2012-04-24
+ *            Initial implementation
+ *)
+
+ interface
+
+uses
+  Classes;
+
+type
+  ThpHistoryList = class(TPersistent)
+  private
+    FItems: TStringList;
+    FMaxLines: Integer;
+  public
+    constructor Create; virtual;
+    destructor Destroy; override;
+    procedure Add(const AIdentifier, ALine: string);
+    procedure Clear(const AIdentifier: string);
+    function Get(const AIdentifier: string): string;
+  published
+    property MaxLines: Integer read FMaxLines write FMaxLines;
+  end;
+
+implementation
+
+{ ThpHistoryList }
+
+constructor ThpHistoryList.Create;
+begin
+  inherited;
+  FItems := TStringList.Create;
+  FItems.Duplicates := dupIgnore;
+  FItems.Sorted := True;
+  FMaxLines := 1000;
+end;
+
+destructor ThpHistoryList.Destroy;
+var
+  i: Integer;
+begin
+  for i := FItems.Count - 1 downto 0 do begin
+    TStringList(FItems.Objects[i]).Clear;
+    TStringList(FItems.Objects[i]).Free;
+    FItems.Delete(i);
+  end;
+  inherited;
+end;
+
+procedure ThpHistoryList.Add(const AIdentifier, ALine: string);
+var
+  i: Integer;
+  sl: TStringList;
+begin
+  i := FItems.IndexOf(AIdentifier);
+  if i = -1 then begin
+    sl := TStringList.Create;
+    i := FItems.AddObject(AIdentifier, sl);
+  end;
+  TStringList(FItems.Objects[i]).Add(ALine);
+  if TStringList(FItems.Objects[i]).Count > FMaxLines then
+    TStringList(FItems.Objects[i]).Delete(0);
+end;
+
+procedure ThpHistoryList.Clear(const AIdentifier: string);
+var
+  i: Integer;
+begin
+  i := FItems.IndexOf(AIdentifier);
+  if i > -1 then
+    TStringList(FItems.Objects[i]).Clear;
+end;
+
+function ThpHistoryList.Get(const AIdentifier: string): string;
+var
+  i: Integer;
+begin
+  i := FItems.IndexOf(AIdentifier);
+  if i > -1 then
+    Result := TStringList(FItems.Objects[i]).Text;
+end;
+
+end.
